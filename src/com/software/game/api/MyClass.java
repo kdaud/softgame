666a7ea89fa2d53f5eae3b2de048a4877762625e
kdaud <dkakumirizii@gmail.com>
2021-01-21 16:22:21 +0300

    added some coded api

---
+++ src/com/software/game/api/MyClass.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package com.software.game.api;
+
+
+/**
+ * @author kdaud
+ */
+public class MyClass {
+	
+	private int age;
+	
+	private String name;
+	
+	private boolean elligibility;
+	
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the elligibility
+	 */
+	public boolean isElligibility() {
+		return elligibility;
+	}
+	
+	/**
+	 * @param elligibility the elligibility to set
+	 */
+	public void setElligibility(boolean elligibility) {
+		this.elligibility = elligibility;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "MyClass [age= " + age + ", name= " + name + ", elligibility= " + elligibility + "]";
+	}
+	
+	/**
+	 * 
+	 * @param age
+	
+	public void setAge(int age) {
+		this.age = age;
+		
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	 */
+	
+
+}
---
+++ src/com/software/game/api/OpenClass.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.software.game.api;
+
+
+/**
+ * @author opensource
+ *
+ */
+public class OpenClass {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MyClass obj = new MyClass();
+		obj.setAge(28);
+		obj.setName("Kakumirizi");
+		obj.setElligibility(true);
+		System.out.println(obj.toString());
+		
+	}
+	
+}
